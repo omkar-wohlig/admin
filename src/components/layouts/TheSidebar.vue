34456f95e3b51dea40bc014d4f99ed173dcacc57
omkar-wohlig <omkarwohlig@gmail.com>
2022-01-24 17:30:50 +0530

    sidebar

--- src/components/layouts/TheSidebar.vue
+++ src/components/layouts/TheSidebar.vue
@@ -25,7 +25,7 @@
                   d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                 />
               </svg>
-              Dashboard
+              Jobs
             </a>
           </li>
         </router-link>
@@ -52,7 +52,7 @@
                 d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"
               />
             </svg>
-            Reports
+            Candidates
           </a>
         </li>
 
