e0cc3c4f13762095f7661001b7716835549a3e33
divC2001 <diveshwohlig@gmail.com>
2022-01-24 17:30:38 +0530

    Changes

--- src/components/layouts/TheSidebar.vue
+++ src/components/layouts/TheSidebar.vue
@@ -116,7 +116,6 @@
                       </span>
                     </DisclosureButton>
                     <DisclosurePanel>
-                      <ul>
                         <router-link v-slot="{ isExactActive, href, navigate }" to="/users" custom>
                           <li
                             class="pl-12"
@@ -148,10 +147,27 @@
                             User Detail
                           </a>
                         </li> -->
-                      </ul>
                     </DisclosurePanel>
                   </Disclosure>
                 </li>
+                <router-link v-slot="{ isExactActive, href, navigate }" to="/users" custom>
+                          <li
+                            class="pl-12"
+                            :class="[isExactActive ? 'bg-gray-500 text-gray-800' : 'hover:bg-gray-700']"
+                          >
+                            <a class="py-3 flex items-center" :href="href" @click="navigate">
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="h-5 w-5"
+                                fill="currentColor"
+                                viewBox="0 0 16 16"
+                              >
+                                <path d="M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
+                              </svg>
+                              User List
+                            </a>
+                          </li>
+                        </router-link>
                 <li class="px-8 hover:bg-gray-700">
                   <a href="#" class="py-3 flex items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="currentColor" viewBox="0 0 16 16">
