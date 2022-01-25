a683bc1578042c221ce0bbbc433e3df06c2889af
omkar-wohlig <omkarwohlig@gmail.com>
2022-01-25 14:55:20 +0530

    changes

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/css/tailwind.css'
 import router from '@/router'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const app = createApp(App)
 app.use(router)
