daccae965950b8479201532806ea8aa759dc301c
divC2001 <diveshwohlig@gmail.com>
2022-01-27 18:02:53 +0530

    changes

--- src/router/index.js
+++ src/router/index.js
@@ -11,9 +11,19 @@ const routes = [
         component: () => import('@/views/dashboard/Jobs/Jobs.vue'),
       },
       {
+        path: '/create',
+        name: 'create-job',
+        component: () => import('@/views/dashboard/Jobs/CreateJob.vue'),
+      },
+      {
         path: 'users',
         name: 'user-list',
         component: () => import('@/views/dashboard/Users/UserList.vue'),
+      },
+      {
+        path: '/users/create',
+        name: 'create-job',
+        component: () => import('@/views/dashboard/Users/CreateUser.vue'),
       },
     ],
   },
