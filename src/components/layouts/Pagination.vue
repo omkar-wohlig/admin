5687ec92da9a1cc60f63e8f08e8e75d6763abc04
divC2001 <diveshwohlig@gmail.com>
2022-01-28 18:18:03 +0530

    changes

--- src/components/layouts/Pagination.vue
+++ src/components/layouts/Pagination.vue
@@ -15,7 +15,6 @@
       this.$emit('page:update', pageNumber);
     },
     totalPages() {
-      console.log(this.userList)
       return Math.ceil(this.userList.length / this.pageSize);
     },
     showPreviousLink() {
